0eec2db34354168bf0e3aab1343bb4e3550decb7
Max <maaxx.360@gmail.com>
2018-07-28 22:19:22 -0500

    Grid array and draw function

--- tiles_test.py
+++ tiles_test.py
@@ -9,16 +9,52 @@ PURPLE = (203, 103, 211)
 ORANGE = (255, 176, 97)
 YELLOW = (238, 226, 0)
 
+
+
+class Grid:
+
+    def __init__(self):
+        self._array = [[0]*4]*4
+        self.size = self.weight, self.height = 100, 100
+        self.init_pos = self.init_x, self.init_y = 0,0
+        self.center_grid()
+        self.tiles_pos = [[], []]
+        self.get_pos()
+
+    # Get initial x and y coordinates of the grid
+    def center_grid(self):
+        center_weight, center_height = (magnitude/2 for magnitude in theGame.size)
+        self.init_x = center_weight - self.weight * 2
+        self.init_y = center_height - self.height * 2
+
+    # Get a list with limiter of the tiles in the grid
+    def get_pos(self):
+        for i in range(len(self._array[0])):
+            self.tiles_pos[0].append(self.init_x + self.weight*(i))
+        for j in range(len(self._array)):
+            self.tiles_pos[1].append(self.init_y + self.height*(j))
+
+    # Draw each tile after knowing it's position
+    def draw(self):
+        for j in range(len(self._array)):
+            for i in range(len(self._array[j])):
+                left, top = self.tiles_pos[0][i], self.tiles_pos[1][j]
+                tile = pygame.Rect(left, top, self.weight, self.height)
+                if self._array[j][i] == 0:
+                    pygame.draw.rect(theGame._display_surf, BLACK, tile, 0)
+
+
 class Game:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+        self.size = self.weight, self.height = 512, 512
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, 0, 32)
         self._running = True
+        self.my_grid = Grid()
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -29,6 +65,7 @@ class Game:
 
     def on_render(self):
         self._display_surf.fill(BLUE)
+        self.my_grid.draw()
         pygame.display.update()
 
     def on_cleanup(self):
@@ -47,5 +84,5 @@ class Game:
         self.on_cleanup()
 
 if __name__ == "__main__" :
-    theApp = Game()
-    theApp.on_execute()
+    theGame = Game()
+    theGame.on_execute()
